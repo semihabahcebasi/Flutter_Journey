d369b3d8e0500e23768c6f72962846ff45408294
Semiha BAHÇEBAŞI <162346635+semihabahcebasi@users.noreply.github.com>
2026-02-22 22:40:55 +0300

    Gün 3: Container dekorasyonu ve bileşen yapısı eklendi

--- calisma_projesi/lib/main.dart
+++ calisma_projesi/lib/main.dart
@@ -28,58 +28,7 @@ class uygulamam extends StatelessWidget {
             //aralarındaki boşluk hizalaması için spaceEvenly, spaceAround, spaceBetween gibi seçenekler var
             //crossAxisAlignment: CrossAxisAlignment.end,  //-->>ögelerin yatayda ortalanması ya da diğer hareketleri için
             children: [
-              Container(
-                //width: 100,
-                //height: 100,
-                // padding: EdgeInsets.all(
-                //   20,
-                //  ), //-->container içindeki öğe ile container kenarı arasındaki boşluk
-                padding: EdgeInsets.symmetric(
-                  vertical: 10,
-                ), //-->container içindeki öğe ile container kenarı arasındaki boşluk
-
-                margin: EdgeInsets.all(
-                  50,
-                ), //-->container ile diğer öğeler arasındaki boşluk
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 243, 33, 33),
-                  borderRadius: BorderRadius.horizontal(
-                    left: Radius.circular(20),
-                  ),
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 255, 255, 255),
-                    width: 3,
-                  ), //-->container kenarına çizgi eklemek için
-                  gradient: LinearGradient(
-                    colors: [
-                      Color.fromARGB(255, 193, 152, 152),
-                      Color.fromARGB(255, 144, 5, 84),
-                    ],
-                    begin: Alignment.topLeft, // gradient başlangıç noktası
-                    end: Alignment.bottomRight, // gradient bitiş noktası
-                  ),
-
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.red.shade500,
-                      blurRadius: 10,
-                      spreadRadius: 5,
-                      offset: Offset(0, 10), // gölgenin konumu (x, y)
-                    ),
-                  ], //-->container gölgelendirme eklemek için
-                  // borderRadius: BorderRadius.circular(20),
-                  //borderRadius:BorderRadius.horizontal(left:Radius.circular(20))  ->> sadece sol köşeleri yuvarlamak için
-                ), //-->container köşelerini yuvarlamak için
-                alignment: Alignment
-                    .center, //-->container içindeki öğeyi ortalamak için
-                child: Text(
-                  "hikayemiz",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w900,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+              compenentUse(),
 
               // image padding vs düzenlemesi yoktur . bunun için dışardan image i padding içine almak gerekir
               Padding(
@@ -140,6 +89,55 @@ class uygulamam extends StatelessWidget {
           ),
         ),
         backgroundColor: Color.fromARGB(255, 235, 193, 217),
+      ),
+    );
+  }
+
+  Container compenentUse() {
+    return Container(
+      //width: 100,
+      //height: 100,
+      // padding: EdgeInsets.all(
+      //   20,
+      //  ), //-->container içindeki öğe ile container kenarı arasındaki boşluk
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+      ), //-->container içindeki öğe ile container kenarı arasındaki boşluk
+
+      margin: EdgeInsets.all(
+        50,
+      ), //-->container ile diğer öğeler arasındaki boşluk
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 243, 33, 33),
+        borderRadius: BorderRadius.horizontal(left: Radius.circular(20)),
+        border: Border.all(
+          color: const Color.fromARGB(255, 255, 255, 255),
+          width: 3,
+        ), //-->container kenarına çizgi eklemek için
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 193, 152, 152),
+            Color.fromARGB(255, 144, 5, 84),
+          ],
+          begin: Alignment.topLeft, // gradient başlangıç noktası
+          end: Alignment.bottomRight, // gradient bitiş noktası
+        ),
+
+        boxShadow: [
+          BoxShadow(
+            color: Colors.red.shade500,
+            blurRadius: 10,
+            spreadRadius: 5,
+            offset: Offset(0, 10), // gölgenin konumu (x, y)
+          ),
+        ], //-->container gölgelendirme eklemek için
+        // borderRadius: BorderRadius.circular(20),
+        //borderRadius:BorderRadius.horizontal(left:Radius.circular(20))  ->> sadece sol köşeleri yuvarlamak için
+      ), //-->container köşelerini yuvarlamak için
+      alignment: Alignment.center, //-->container içindeki öğeyi ortalamak için
+      child: Text(
+        "hikayemiz",
+        style: TextStyle(fontWeight: FontWeight.w900, color: Colors.white),
       ),
     );
   }
